1b1fbf71779807f0ae29ed02edd094d53d5a8212
SangKyun-Seo <tkdrbs0802@naver.com>
2024-08-01 23:21:32 +0900

    fix: repo이름 변경 및 빌드

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath : '/sangkyun.github.io/docs',
+  publicPath : '/sangkyun.github.io/',
   outputDir: 'docs'
 })
