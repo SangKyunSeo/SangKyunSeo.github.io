0eec0501f3597aee2304a5ad03a64fcd9840c176
SangKyun-Seo <tkdrbs0802@naver.com>
2024-08-01 23:33:06 +0900

    fix: 배포 경로 수정

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath : '/SangKyunSeo.github.io/',
+  publicPath : '/',
   outputDir: 'docs'
 })
